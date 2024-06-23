d349a5fb4c1fe542dd78f9950747aadcec96a18b
Mr-D27 <dmoyap@usm.cl>
2024-06-23 17:23:36 -0400

    Projecto Web

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function index(){
+        return view('home.login');
+    }
+}
